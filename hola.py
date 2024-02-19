0bbb1ec01f8e3bdceb0eac473b3e9b48bf419289
AriathZ4 <epsilonculiao@gmail.com>
2024-02-18 22:41:10 -0600

    texto

--- hola.py
+++ hola.py
@@ -1,1 +1,13 @@
-print('q pex')
+import os
+from discord.ext import commands
+from dotenv import load_dotenv
+import urllib.request
+import json
+
+load_dotenv()
+TOKEN = os.getenv("OTQyMjc4NTk2NDEwNjM4Mzc2.GR1iPj.GSddEA0JrLUa_uiP4-yhPzPwwrHI1eIuL_Gyjw")
+
+bot = commands.Bot(command_prefix="-")
+
+"""@bot.command(name="xxxxxx")
+async def xxxxx ()"""
