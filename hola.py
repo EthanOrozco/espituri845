6ac21ffbaad77249316a4082731a105630f9682d
EthanOrozco <111812386+EthanOrozco@users.noreply.github.com>
2024-02-18 23:26:22 -0600

    Update hola.py
    ASAAAA

--- hola.py
+++ hola.py
@@ -2,24 +2,6 @@ import discord
 
 intents = discord.Intents.default()
 intents.message_content = True
-<<<<<<< HEAD
-
-client = discord.Client(intents=intents)
-
-@client.event
-async def on_ready():
-    print(f'We have logged in as {client.user}')
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
-
-client.run('OTQyMjc4NTk2NDEwNjM4Mzc2.G42NFz.a3jVS2iXuGDcNReBF8aHT8pcHs-bIeu8iwLorU')
-=======
 
 client = discord.Client(intents=intents)
 
@@ -46,6 +28,3 @@ TEMPLATE
 async def XXXX(YYYY):
     function here
 """
-
-
->>>>>>> f79dfcf390694f631e83ec1d4479a77263775a15
