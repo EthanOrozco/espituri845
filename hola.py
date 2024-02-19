1c1d63a9e7b0aba2e3ee10b442dcf75e8da2a401
EthanOrozco <111812386+EthanOrozco@users.noreply.github.com>
2024-02-18 22:43:26 -0600

    Update hola.py
    deleted token

--- hola.py
+++ hola.py
@@ -5,7 +5,7 @@ import urllib.request
 import json
 
 load_dotenv()
-TOKEN = os.getenv("OTQyMjc4NTk2NDEwNjM4Mzc2.GR1iPj.GSddEA0JrLUa_uiP4-yhPzPwwrHI1eIuL_Gyjw")
+TOKEN = os.getenv("")
 
 bot = commands.Bot(command_prefix="-")
 
